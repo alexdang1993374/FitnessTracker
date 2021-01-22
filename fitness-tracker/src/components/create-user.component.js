03d5b7daaccff685bc6aa610a4f95016861ea28b
unknown <alexdang1993374@gmail.com>
2021-01-23 03:50:16 +0700

    create user connected to database

--- fitness-tracker/src/components/create-user.component.js
+++ fitness-tracker/src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class CreateUser extends Component {
   constructor(props) {
@@ -26,6 +27,10 @@ export default class CreateUser extends Component {
     };
 
     console.log(user);
+
+    axios
+      .post("http://localhost:5000/api/users", user)
+      .then((res) => console.log(res.data));
 
     this.setState({
       username: "",
