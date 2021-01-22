deda22fa9cf615e0d0aeee225a1303f6a9e752eb
unknown <alexdang1993374@gmail.com>
2021-01-23 05:05:55 +0700

    finished exercise list component

--- fitness-tracker/src/components/exercises-list.component.js
+++ fitness-tracker/src/components/exercises-list.component.js
@@ -1,11 +1,89 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Exercise = (props) => (
+  <tr>
+    <td>{props.exercise.username}</td>
+    <td>{props.exercise.description}</td>
+    <td>{props.exercise.duration}</td>
+    <td>{props.exercise.date.substring(0, 10)}</td>
+    <td>
+      <Link to={"/edit/" + props.exercise.id}>edit</Link> |{" "}
+      <a
+        href="#"
+        onClick={() => {
+          props.deleteExercise(props.exercise.id);
+        }}
+      >
+        delete
+      </a>
+    </td>
+  </tr>
+);
 
 export default class ExercisesList extends Component {
-    render() {
-        return (
-            <>
-                <h1>This is the exercises list component</h1>
-            </>
-        )
-    }
+  constructor(props) {
+    super(props);
+
+    this.deleteExercise = this.deleteExercise.bind(this);
+
+    this.state = { exercises: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/exercises")
+      .then((res) => {
+        console.log(res.data.data);
+        this.setState({
+          exercises: res.data.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  deleteExercise(id) {
+    axios
+      .delete("http://localhost:5000/api/exercises/" + id)
+      .then((res) => console.log(res.data));
+
+    this.setState({
+      exercises: this.state.exercises.filter((el) => el.id !== id),
+    });
+  }
+
+  exerciseList() {
+    return this.state.exercises.map((currentExercise) => {
+      return (
+        <Exercise
+          exercise={currentExercise}
+          deleteExercise={this.deleteExercise}
+          key={currentExercise.id}
+        />
+      );
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <h3>Logged Exercises</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Duration</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{this.exerciseList()}</tbody>
+        </table>
+      </>
+    );
+  }
 }
